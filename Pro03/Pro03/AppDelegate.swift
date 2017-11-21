73eea3e8c1a9c267306aaf3371aa4a5c73b68480
nerd010 <markwang559@gmail.com>
2017-11-21 17:13:47 +0800

    add pro03

---
+++ Pro03/Pro03/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Pro03
+//
+//  Created by richard on 21/11/2017.
+//  Copyright © 2017 abc. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
+
