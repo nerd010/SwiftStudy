32aefe45e94f6c68c1ec4a75dc740917d018e52e
nerd010 <markwang559@gmail.com>
2017-11-24 14:38:43 +0800

    add some files

---
+++ Pro05/Pro05/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Pro05
+//
+//  Created by richard on 23/11/2017.
+//  Copyright © 2017 abc. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    UINavigationBar.appearance().tintColor = UIColor.white
+    return true
+  }
+}
+
